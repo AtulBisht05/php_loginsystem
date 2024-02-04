c3c18c9a0a3fd4316713e9dbeafff270f5381463
Atul <atulbisht02@gmail.com>
2024-02-04 18:15:10 +0530

    update work progress

--- login.php
+++ login.php
@@ -1,4 +1,80 @@
 <?php
+$login = false;
+$showError = false;
 
-    echo "login-page";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    require 'comp/dbconnect.php';
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM `users` WHERE `username`='$username' AND `password`='$password'";
+    $result = mysqli_query($conn, $sql);
+    $num = mysqli_num_rows($result);
+    if ($num == 1) {
+        $login = true;
+        session_start();
+        $_SESSION['loggedin'] = true;
+        $_SESSION['usename'] = $username;
+        header("location:home.php");
+    }
+} else {
+    $showError = true;
+}
 ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <?php require 'comp/nav.php'; ?>
+
+    <?php
+    if ($login) {
+        echo "
+        <div class='alert alert-success alert-dismissible fade show' role='alert'>
+        <strong> Success!</strong> You have sucessfully Login.
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+        </div>";
+    }
+    if ($showError) {
+        echo "
+        <div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        <strong> Error!</strong> User not exist.
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+        </div>";
+    }
+    ?>
+
+    <div class="container">
+        <h1 class="text-center">Login</h1>
+        <form action='/php_loginsystem/login.php' method='post'>
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="email" class="form-control" name="username" id="username" aria-describedby="emailHelp">
+
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" id="password">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
